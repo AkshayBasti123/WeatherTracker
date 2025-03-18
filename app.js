1d6da5328876b518c46de4d16c3ed51e4a01397d
AkshayBasti <70245648+AkshayBasti123@users.noreply.github.com>
2025-03-17 22:41:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector("button");
+    const button = document.querySelector("#searchBtn");
     const input = document.querySelector("input");
 
     button.addEventListener("click", function () {
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (location) {
             fetchWeather(location);
+            fetchForecast(location);
         } else {
             displayError("Please enter a valid location.");
         }
@@ -32,6 +33,25 @@ function fetchWeather(location) {
         });
 }
 
+function fetchForecast(location) {
+    const apiKey = "b1eeafd271ab3faa4a3c948fe3387fcf"; 
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Could not retrieve forecast");
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayForecast(data);
+        })
+        .catch(error => {
+            displayError(error.message);
+        });
+}
+
 function displayWeather(data) {
     document.querySelector(".weather-info").innerHTML = `
         <h3>${data.name}, ${data.sys.country}</h3>
@@ -42,6 +62,29 @@ function displayWeather(data) {
     `;
 }
 
+function displayForecast(data) {
+    let forecastHTML = "<h3>5-Day Forecast</h3><div class='forecast-container'>";
+
+    // OpenWeatherMap provides data every 3 hours, we filter to get 1 per day
+    const dailyForecasts = data.list.filter((reading, index) => index % 8 === 0);
+
+    dailyForecasts.forEach(day => {
+        const date = new Date(day.dt * 1000).toLocaleDateString("en-US", { weekday: "long" });
+        forecastHTML += `
+            <div class="forecast">
+                <p>${date}</p>
+                <img class="weather-icon" src="https://openweathermap.org/img/wn/${day.weather[0].icon}.png" alt="Weather icon">
+                <p>${day.weather[0].description}</p>
+                <p>🌡 ${day.main.temp}°C</p>
+            </div>
+        `;
+    });
+
+    forecastHTML += "</div>";
+    document.querySelector(".forecast-info").innerHTML = forecastHTML;
+}
+
 function displayError(message) {
     document.querySelector(".weather-info").innerHTML = `<p class="error">${message}</p>`;
+    document.querySelector(".forecast-info").innerHTML = ""; // Clear forecast section
 }
